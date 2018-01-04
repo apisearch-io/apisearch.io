c2cce9e62a54a0ac8c89a060adac11875fc94f46
alexhoma <alexcm.14@gmail.com>
2018-01-04 14:13:28 +0100

    wip(integrations)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import 'styles/index.sass';
 
-(function() {
+import hljs from "highlight.js";
+import apisearchUI from "apisearch-ui";
+
+hljs.configure({useBR: true});
+
+
+const addNavbarClassOnScroll = () => {
     window
         .addEventListener('scroll', () => {
             let navbar = document.querySelector('#primaryNavbar');
@@ -12,4 +18,44 @@ import 'styles/index.sass';
             }
         })
     ;
+};
+
+const integrationsApisearchDemo = () => {
+    let ui = apisearchUI({
+        appId: '54725861',
+        index: '66777162',
+        token: 'daf93c2b-40bc-49f2-870e-f8f62ea524ad'
+    });
+
+    ui.addWidgets(
+        ui.widgets.simpleSearch({
+            target: '.as-integrationDemo__searchBox',
+        }),
+        ui.widgets.result({
+            target: '.as-integrationDemo__results',
+            itemsPerPage: 3,
+            template: {
+                itemsList: `<ul>
+                    {{#items}} 
+                    <li>
+                        <img src="{{metadata.img}}" height="40px">
+                        {{metadata.title}}
+                    </li> 
+                    {{/items}}
+                </ul>`,
+            }
+        })
+    );
+
+    // Initialize it!
+    ui.init();
+};
+
+(function() {
+    document
+        .querySelectorAll('code')
+        .forEach(block => hljs.highlightBlock(block))
+    ;
+    integrationsApisearchDemo();
+    addNavbarClassOnScroll();
 })();
