22fc321b2553c07f99cc03afefee19b722c629d3
Marc Morera <yuhu@mmoreram.com>
2021-02-11 18:42:20 +0100

    Added dynamic layer on top of the website
    - Generated code under /docs folder
    - Execute code generation with `php generate.php`
    - Added twig
    - Generates multiple languages with YAML files

---
+++ generate.php
@@ -0,0 +1,87 @@
+<?php
+
+use Symfony\Component\Yaml\Yaml;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require "vendor/autoload.php";
+
+$twig = new Environment(new FilesystemLoader([
+    __DIR__ . '/templates'
+]));
+
+$config = Yaml::parse(file_get_contents(__DIR__ . '/config.yml'));
+$config['assets'] = trim($config['assets'] ?? 'assets', '/');
+$config['target'] = trim($config['target'] ?? 'public', '/');
+$config['languages'] = $config['languages'] ?? 'en';
+
+
+$target = trim($config['target'], '/');
+$languages = $config['languages'];
+$pages = $config['pages'];
+
+exec("rm -Rf " . __DIR__ . "/$target");
+mkdir(__DIR__ . "/$target");
+
+foreach ($languages as $language) {
+    $languageTranslations = Yaml::parse(file_get_contents(__DIR__ . "/languages/$language.yml"));
+    if ($language !== $config['root_language']) {
+        mkdir(__DIR__ . "/$target/$language");
+    }
+    foreach ($pages as $page) {
+        generatePageInLanguage(
+            $twig,
+            $page,
+            $language,
+            $languageTranslations[$page] ?? [],
+            $config
+        );
+    }
+}
+
+copyResources($config);
+
+/**
+ * @param Environment $twig
+ * @param string      $page
+ * @param string      $language
+ * @param array       $translations
+ * @param array       $config
+ */
+function generatePageInLanguage(
+    Environment $twig,
+    string $page,
+    string $language,
+    array $translations,
+    array $config
+)
+{
+    $target = $config['target'];
+    $assets = $config['assets'];
+    $isRootLanguage = $language === $config['root_language'];
+    $rootPath = $isRootLanguage ? '.' : '..';
+    $languagePath = $isRootLanguage
+        ? ''
+        : "/$language";
+
+    $content = $twig->render("$page.twig", [
+        'config' => $config,
+        't' => $translations,
+        'root_path' => $rootPath,
+        'assets_path' => $rootPath . '/' . $assets,
+    ]);
+
+    file_put_contents(__DIR__ . "/{$target}{$languagePath}/$page.html", $content);
+}
+
+/**
+ * @param array $config
+ */
+function copyResources(array $config)
+{
+    $target = $config['target'];
+    $assets = $config['assets'];
+    $sourcePath = __DIR__ . "/$assets";
+    $targetPath = __DIR__ . "/$target/$assets";
+    exec("cp -R $sourcePath $targetPath");
+}
